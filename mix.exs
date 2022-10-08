28405991ae8a1e0dec13f1fe48f4071e6b4434e1
Chris Nelson <superchrisnelson@gmail.com>
2022-10-08 17:34:25 -0400

    adding livestate and stripe

--- mix.exs
+++ mix.exs
@@ -48,7 +48,11 @@ defmodule StripeCart.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:live_state, "~> 0.6.0"},
+      {:cors_plug, "~> 3.0"},
+      {:stripity_stripe, "~> 2.17.1"},
+      {:cachex, "~> 3.4.0"}
     ]
   end
 

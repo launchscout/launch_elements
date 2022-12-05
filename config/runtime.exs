d760529eec4e030e9015e411f35f96000c364db8
Chris Nelson <superchrisnelson@gmail.com>
2022-12-05 18:02:18 -0500

    dont need no stinkin badges err origin checking

--- config/runtime.exs
+++ config/runtime.exs
@@ -51,6 +51,7 @@ if config_env() == :prod do
 
   config :stripe_cart, StripeCartWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
+    check_origin: false,
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.

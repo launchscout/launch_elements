2f7934b9c4cf5e9371391694579bac120a742e2d
Chris Nelson <superchrisnelson@gmail.com>
2022-12-05 15:14:59 -0500

    follow instructions for render

--- config/runtime.exs
+++ config/runtime.exs
@@ -16,9 +16,7 @@ import Config
 #
 # Alternatively, you can use `mix phx.gen.release` to generate a `bin/server`
 # script that automatically sets the env var above.
-if System.get_env("PHX_SERVER") do
-  config :stripe_cart, StripeCartWeb.Endpoint, server: true
-end
+config :stripe_cart, StripeCartWeb.Endpoint, server: true
 
 if config_env() == :prod do
   database_url =
@@ -48,18 +46,15 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
-  port = String.to_integer(System.get_env("PORT") || "4000")
-
   config :stripe_cart, StripeCartWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
+    url: [host: System.get_env("RENDER_EXTERNAL_HOSTNAME") || "localhost", port: 80],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
+      port: 80
     ],
     secret_key_base: secret_key_base
 

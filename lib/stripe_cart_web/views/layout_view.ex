ebe44aacb488942e43b18babb5ba67d4ddfed2d9
Katie Pohlman <katie@gaslight.co>
2023-01-18 16:19:42 -0500

    WIP mobile navigation

--- lib/stripe_cart_web/views/layout_view.ex
+++ lib/stripe_cart_web/views/layout_view.ex
@@ -1,7 +1,15 @@
 defmodule StripeCartWeb.LayoutView do
   use StripeCartWeb, :view
 
+  alias Phoenix.LiveView.JS
+
   # Phoenix LiveDashboard is available only in development by default,
   # so we instruct Elixir to not warn if the dashboard route is missing.
   @compile {:no_warn_undefined, {Routes, :live_dashboard_path, 2}}
+
+  defp toggle_class(js \\ %JS{}, clazz, selector) do
+    js
+    |> JS.remove_class(clazz, to: selector)
+    |> JS.add_class(clazz, to: selector <> ":not(.#{clazz})")
+  end
 end

23af0bbfbfdd8f58a8370c26a202c5ed9a1b32bf
Katie Pohlman <katie@gaslight.co>
2023-01-19 10:25:43 -0500

    Finish up mobile navigation

--- assets/js/nav-toggle.js
+++ assets/js/nav-toggle.js
@@ -2,6 +2,7 @@ class NavToggle extends HTMLElement {
   connectedCallback() {
     this.addEventListener("click", () => {
       document.querySelector(".nav").classList.toggle("open")
+      document.querySelector(".header").classList.toggle("open")
     });
   }
 }
--- lib/stripe_cart_web/views/layout_view.ex
+++ lib/stripe_cart_web/views/layout_view.ex
@@ -1,14 +1,6 @@
 defmodule StripeCartWeb.LayoutView do
   use StripeCartWeb, :view
 
-  alias Phoenix.LiveView.JS
-
-  defp toggle_class(js \\ %JS{}, clazz, selector) do
-    js
-    |> JS.remove_class(clazz, to: selector)
-    |> JS.add_class(clazz, to: selector <> ":not(.#{clazz})")
-  end
-
   # Phoenix LiveDashboard is available only in development by default,
   # so we instruct Elixir to not warn if the dashboard route is missing.
   @compile {:no_warn_undefined, {Routes, :live_dashboard_path, 2}}

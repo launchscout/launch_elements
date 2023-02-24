1bf853666b9f4666290cfdb4c8e6f2e9640fa61d
Katie Pohlman <katie@gaslight.co>
2023-02-24 11:50:33 -0500

    Finish first draft of docs

--- assets/js/launch-cart.ts
+++ assets/js/launch-cart.ts
@@ -180,7 +180,7 @@ export class LaunchCartElement extends LitElement {
         <button id="checkout-button" part="checkout-button" @click=${this.checkout}>
           ${this.checkingOut ? html`
             <svg id="checkout-spinner" part="spinner" viewBox="0 0 50 50">
-              <circle part="path" cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>
+              <circle part="spinner-path" cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>
             </svg>
             ` : ''} Check out
         </button>

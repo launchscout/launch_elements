c911aca1c94520396286d323c3c17e4749b48ecd
Chris Nelson <superchrisnelson@gmail.com>
2023-02-28 15:15:13 -0500

    use mailgun

--- config/runtime.exs
+++ config/runtime.exs
@@ -68,10 +68,11 @@ if config_env() == :prod do
   # Also, you may need to configure the Swoosh API client of your choice if you
   # are not using SMTP. Here is an example of the configuration:
   #
-  #     config :launch_cart, LaunchCart.Mailer,
-  #       adapter: Swoosh.Adapters.Mailgun,
-  #       api_key: System.get_env("MAILGUN_API_KEY"),
-  #       domain: System.get_env("MAILGUN_DOMAIN")
+  config :live_state_comments, LiveStateComments.Mailer,
+    adapter: Swoosh.Adapters.Mailgun,
+    api_key: System.get_env("MAILGUN_API_KEY"),
+    domain: "mg.gaslight.co"
+
   #
   # For this example you need include a HTTP client required by Swoosh API client.
   # Swoosh supports Hackney and Finch out of the box:

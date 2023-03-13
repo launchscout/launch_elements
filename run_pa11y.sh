1dda761d2070eac2065cc0ea5683a54334fe3ba4
Andrew Moore <andrew@lessthanseventy.com>
2023-03-13 00:41:27 -0600

    report errors

--- run_pa11y.sh
+++ run_pa11y.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
 DIR=/home/runner/work/launch_cart/launch_cart/test/axe_html/
+DIR=test/axe_html/
+STATUS=0
+
+pa11y_test() {
+	sed -i 's|/assets/css/app.css|./assets/css/app.css|g' $1
+	sed -i 's|/assets/js/app.js|./assets/js/app.js|g' $1
+	node assets/node_modules/pa11y/bin/pa11y.js $1
+	local -i status=($?)
+	if ((status > 0)); then
+		STATUS=($(echo $status))
+	fi
+}
 
 for f in $(find $DIR -iname "*.html"); do
 	if [ ! -d "$f" ]; then
-		sed -i 's|/assets/css/app.css|./assets/css/app.css|g' $f
-		sed -i 's|/assets/js/app.js|./assets/js/app.js|g' $f
-		node assets/node_modules/pa11y/bin/pa11y.js $f
+		pa11y_test $f
 	fi
 done
+
+(exit $STATUS)
